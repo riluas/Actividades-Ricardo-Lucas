dc02a9614afe6ca848b9e916826a1b383d656d3f
riluas <44229797+riluas@users.noreply.github.com>
2019-04-16 13:43:55 +0200

    Add files via upload

--- Proyecto 9/Juegosbd Ricardo Lucas.sql
+++ Proyecto 9/Juegosbd Ricardo Lucas.sql
@@ -1,74 +1,81 @@
--- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 30-01-2019 a las 23:13:11
--- Versión del servidor: 10.1.34-MariaDB
--- Versión de PHP: 5.6.37
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `juegosbd`
---
-CREATE DATABASE IF NOT EXISTS `juegosbd` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `juegosbd`;
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.1.32-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win32
+-- HeidiSQL Versión:             9.5.0.5196
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `usuario`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+
+-- Volcando estructura de base de datos para juegosbd
+CREATE DATABASE IF NOT EXISTS `juegosbd` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `juegosbd`;
+
+-- Volcando estructura para tabla juegosbd.juego
+CREATE TABLE IF NOT EXISTS `juego` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- Volcando datos para la tabla juegosbd.juego: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `juego` DISABLE KEYS */;
+INSERT IGNORE INTO `juego` (`id`, `nombre`) VALUES
+	(1, 'Microrrobots'),
+	(2, 'Parchis');
+/*!40000 ALTER TABLE `juego` ENABLE KEYS */;
+
+-- Volcando estructura para tabla juegosbd.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `apellidos` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `edad` int(3) NOT NULL,
   `curso` int(5) NOT NULL,
-  `puntuacion` int(4) NOT NULL
+  `puntuacion` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- Volcando datos para la tabla juegosbd.usuario: ~14 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT IGNORE INTO `usuario` (`id`, `nombre`, `apellidos`, `edad`, `curso`, `puntuacion`) VALUES
+	(1, 'David', 'Victoria Martinez', 300, 1, 50),
+	(2, 'Borja', 'Hervas Carapollo', 80, 3, 100),
+	(3, 'Ricardo', 'Lucas Asins', 19, 1, 80),
+	(4, 'Alberto', 'Salinas Asins', 50, 4, 42),
+	(58, 'edf', 'asd', 45, 3, 5),
+	(59, 'edf', 'asd', 45, 3, 5),
+	(60, 'edf', 'asd', 45, 3, 5),
+	(61, 'PACO', 'Perez', 22, 3, 50),
+	(62, 'atun', 'salmon salmon', 45, 3, 41),
+	(63, 'Paco', 'Sanz', 33, 2, 20),
+	(64, 'Ricardo', 'Lucas Asins', 12, 2, 0),
+	(65, 'Ricardo', 'Lucas Asins', 123, 3, 0),
+	(66, 'sdfhi', 'as', 4, 5, 0),
+	(67, 'ss', 'ss', 12, 32, 0);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+
+-- Volcando estructura para tabla juegosbd.usuariojuego
+CREATE TABLE IF NOT EXISTS `usuariojuego` (
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_juego` int(11) DEFAULT NULL,
+  `puntuacion` int(11) DEFAULT '0',
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_juego` (`id_juego`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Volcado de datos para la tabla `usuario`
---
+-- Volcando datos para la tabla juegosbd.usuariojuego: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuariojuego` DISABLE KEYS */;
+INSERT IGNORE INTO `usuariojuego` (`id_usuario`, `id_juego`, `puntuacion`) VALUES
+	(1, 1, 0),
+	(1, 2, 0);
+/*!40000 ALTER TABLE `usuariojuego` ENABLE KEYS */;
 
-INSERT INTO `usuario` (`id`, `nombre`, `apellidos`, `edad`, `curso`, `puntuacion`) VALUES
-(1, 'David', 'Victoria Martinez', 300, 1, 50),
-(2, 'Borja', 'Hervas Carapollo', 80, 3, 100),
-(3, 'Ricardo', 'Lucas Asins', 19, 1, 80);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
